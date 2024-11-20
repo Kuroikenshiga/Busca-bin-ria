9080020511d7cbff689fc28992e58e97df7dfa94
Kuroikenshiga <juniorsilvapalafoz@gmail.com>
2024-11-20 12:06:10 -0300

    20-11-24

--- main.c
+++ main.c
@@ -4,12 +4,9 @@
 int binarySearch(int value, int array[], int size);
 int recusiveBinarySearch(int value, int array[], int first, int last, int size);
 int main() {
-	int array[100];
-	for (int i = 0; i < sizeof(array) / sizeof(array[0]);i++) {
-		array[i] = i;
-	}
+	int array[] = { 1,2,3,7,10 };
 
-	printf("%d", binarySearch(56, array, (sizeof(array) / sizeof(array[0]))));
+	printf("%d", binarySearch(-7, array, (sizeof(array) / sizeof(array[0]))));
 
 	return 0;
 }
@@ -17,15 +14,15 @@ int binarySearch(int value, int array[], int size) {
 	return recusiveBinarySearch(value, array, 0, size, size);
 }
 int recusiveBinarySearch(int value, int array[], int first, int last, int size) {
-	int pointer = (last + first) / 2;
+	int pointer = ((last + first) / 2);
 	if (array[pointer] == value) {
-		return (last + first) / 2;
+		return pointer;
 	}
-	if (pointer == 0 || pointer == size-1){
+	if (first > last) {
 		return -1;
 	}
 	if (array[pointer] < value) {
-		return recusiveBinarySearch(value, array, pointer, last,size);
+		return recusiveBinarySearch(value, array, pointer+1, last,size);
 	}
-	return recusiveBinarySearch(value, array, first, pointer,size);
+	return recusiveBinarySearch(value, array, first, pointer-1,size);
 }
